29be40387771848261eca1f3dc8b705c03450604
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-03-16 18:10:17 +0530

    vehicles page theming

--- app/vehicles/page.jsx
+++ app/vehicles/page.jsx
@@ -1,5 +1,21 @@
+import vehicles from "@/vehicles.json";
+import VehicleCard from "@/components/VehicleCard";
 const VehiclesPage = () => {
-  return <div>Vehicles Page</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {vehicles.length === 0 ? (
+          <p>No vehicles found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {vehicles.map((item) => (
+              <VehicleCard vehicle={item} key={vehicles._id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default VehiclesPage;
