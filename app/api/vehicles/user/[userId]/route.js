f338fba941f7ea2eec2e7e677d46a27fe0914f60
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-03-22 19:57:06 +0530

    user profile display, user vehicle listing

---
+++ app/api/vehicles/user/[userId]/route.js
@@ -0,0 +1,24 @@
+import connectDB from "@/config/db";
+import Vehicle from "@/models/Vehicle";
+
+//GET /api/vehicles/user/:userid
+export const GET = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const userId = params.userId; //foldername=userId
+    console.log(userId);
+    if (!userId) {
+      return new Response("User ID requires", { status: 400 });
+    }
+
+    const vehicles = await Vehicle.find({ owner: userId });
+    console.log(vehicles);
+    return new Response(JSON.stringify(vehicles), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Error", {
+      status: 500,
+    });
+  }
+};
