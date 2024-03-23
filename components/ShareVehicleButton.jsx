f4a8042f3c07441f67a2c39b48321d893c0a8d11
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-03-23 18:19:48 +0530

    social share button:react-share

--- components/ShareVehicleButton.jsx
+++ components/ShareVehicleButton.jsx
@@ -1,8 +1,53 @@
-const ShareVehicleButton = () => {
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  EmailIcon,
+  WhatsappIcon,
+} from "react-share";
+const ShareVehicleButton = ({ vehicle }) => {
+  const url = `${process.env.NEXT_PUBLIC_DOMAIN}/vehicles/${vehicle._id}`;
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      Share Vehicle
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        Share This Vehicle:
+      </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={url}
+          quote={vehicle.name}
+          hashtag={
+            `#${vehicle.type.replace(
+              /\s/g,
+              ""
+            )}ForRent` /* regular expression for replacing space with emptystring */
+          }
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={url}
+          title={vehicle.name}
+          hashtags={[`${vehicle.type.replace(/\s/g, "")}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+        <WhatsappShareButton url={url} title={vehicle.name} separator=":: ">
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+
+        <EmailShareButton
+          url={url}
+          subject={vehicle.name}
+          body={`Vehicle For rent: ${url}`}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 
