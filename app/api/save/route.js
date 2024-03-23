b4d3e45ab3caaad845df9888a2205f43bb744997
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-03-23 15:54:26 +0530

    save route added, making req from savebutton

---
+++ app/api/save/route.js
@@ -0,0 +1,52 @@
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import Vehicle from "@/models/Vehicle";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic =
+  "force-dynamic"; /* route segment config(nexjs) : bug fix in deployment */
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const { vehicleId } = await request.json(); //taking the vehicleid from requestbody , becuase we can't take it from params since we are not padding params in route
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // Check if vehicle is already bookmarked/saved
+    let isBookmarked = user.bookmarks.includes(vehicleId);
+
+    let message;
+
+    if (isBookmarked) {
+      // If already bookmarked, remove it
+      user.bookmarks.pull(vehicleId);
+      message = "Vehicle deleted successfully";
+      isBookmarked = false;
+    } else {
+      // If not bookmarked, add it
+      user.bookmarks.push(vehicleId);
+      message = "Vehcle saved successfully";
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
--- components/SaveVehicleButton.jsx
+++ components/SaveVehicleButton.jsx
@@ -1,8 +1,45 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { toast } from "react-toastify";
 import { FaBookmark } from "react-icons/fa";
 const SaveVehicleButton = ({ vehicle }) => {
+  const { data: session } = useSession();
+
+  const userId = session?.user?.id;
+
+  const [isSaved, setIsSaved] = useState(false);
+
+  const handleSave = async () => {
+    if (!userId) {
+      toast.error("Sign In to Save vehicle");
+      return;
+    }
+    try {
+      const res = await fetch("/api/save/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ vehicleId: vehicle._id }),
+      });
+      if (res.status === 200) {
+        const data = await res.json();
+        toast.success(data.message); //from route.js backend
+        setIsSaved(data.isBookmarked);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <>
-      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+      <button
+        onClick={handleSave}
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      >
         <FaBookmark className="fas fa-share mr-2" /> Bookmark Vehicle
       </button>
     </>
