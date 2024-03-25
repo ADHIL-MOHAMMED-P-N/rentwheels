db204b59a1279b943449347af92dbbb446331531
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-03-25 23:55:32 +0530

    lightbox photoswipe

--- components/VehicleImages.jsx
+++ components/VehicleImages.jsx
@@ -1,47 +1,71 @@
 import Image from "next/image";
-
+import { Gallery, Item } from "react-photoswipe-gallery";
 const VehicleImages = ({ images }) => {
   return (
-    <section className="bg-blue-50 p-4">
-      <div className="container mx-auto">
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt="vehicle"
-            className="object-cover h-[400px] mx-auto rounded-xl"
-            width={1800}
-            height={400}
-            priority={true}
-          />
-        ) : (
-          /* if there are 3 images , make the third iamge full width in (span 2 cols) */
-          <div className="grid grid-cols-2 gap-4">
-            {images.map((image, index) => (
-              <div
-                className={`
+    <Gallery>
+      <section className="bg-blue-50 p-4">
+        <div className="container mx-auto">
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width="1024"
+              height="768"
+            >
+              {({ ref, open }) => (
+                <Image
+                  ref={ref}
+                  onClick={open}
+                  src={images[0]}
+                  alt="vehicle"
+                  className="object-cover h-[400px] mx-auto rounded-xl"
+                  width={1800}
+                  height={400}
+                  priority={true}
+                />
+              )}
+            </Item>
+          ) : (
+            /* if there are 3 images , make the third iamge full width in (span 2 cols) */
+            <div className="grid grid-cols-2 gap-4">
+              {images.map((image, index) => (
+                <div
+                  className={`
                             ${
                               images.length === 3 && index === 2
                                 ? "col-span-2"
                                 : "col-span-1"
                             }
                             `}
-                key={index}
-              >
-                <Image
-                  src={image}
-                  alt="vehicle"
-                  className="object-cover h-[400px] w-full rounded-xl"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  priority={true}
-                />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+                  key={index}
+                >
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width="1024"
+                    height="768"
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        ref={ref}
+                        onClick={open}
+                        src={image}
+                        alt="vehicle"
+                        className="object-cover h-[400px] w-full rounded-xl"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        priority={true}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   );
 };
 
